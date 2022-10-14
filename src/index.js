3020887f66160f0bab905a7f89e941548b87dc69
tonaak <toannguyenitit@gmail.com>
2022-10-15 00:29:20 +0700

    fixed sw err

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from './App';
 import { store, persistor } from './store/store';
 import { stripePromise } from './utils/stripe/stripe.utils';
 
-import * as serviceWorker from './service-worker';
+import * as serviceWorker from './serviceWorkerRegistration';
 
 import './index.scss';
 
